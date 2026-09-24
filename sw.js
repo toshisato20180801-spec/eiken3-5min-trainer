838c6bf4a966a57d35f60fd4e8d76517425b23bc
toshisato20180801-spec <toshisato20180801@gmail.com>
2026-09-24 19:59:39 +0900

    Add service worker

---
+++ sw.js
@@ -0,0 +1,1 @@
+const CACHE='eiken3-v2';const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
